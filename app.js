f7b7d6b1e922fd5db02675fc8dc25f4b7e3dd9be
goutham1646 <81221500+goutham1646@users.noreply.github.com>
2025-11-29 22:40:00 +0530

    app.js modification

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 // app.js
 const http = require('http');
 const port = process.env.PORT || 3000;
+
 const server = http.createServer((req, res) => {
   res.end('Hello from sample app');
 });
-server.listen(port, ()=>console.log('listening', port));
+
+server.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on port ${port}`);
+});
 
